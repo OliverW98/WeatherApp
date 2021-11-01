ee9096a5e273e69406236a21955b8a114a721415
Oliverw98 <oliverwilkes22@gmail.com>
2021-11-01 17:56:08 +0000

    Looping dailybox components

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import React, { useState } from "react";
 import DailyBox from "./DailyBox";
 import ExtendedDailyBox from "./ExtenededDailyBox";
 function Daily(props) {
@@ -14,52 +14,17 @@ function Daily(props) {
     );
   }
 
+  const dailyBoxes = props.data.map((day, index) => (
+    <DailyBox
+      id={index}
+      data={day}
+      isUsingMetric={props.isUsingMetric}
+      handleClick={handleClick}
+    />
+  ));
   return (
     <div>
-      <div className="columns is-gapless">
-        <DailyBox
-          id={0}
-          data={props.data[0]}
-          isUsingMetric={props.isUsingMetric}
-          handleClick={handleClick}
-        />
-        <DailyBox
-          id={1}
-          data={props.data[1]}
-          isUsingMetric={props.isUsingMetric}
-          handleClick={handleClick}
-        />
-        <DailyBox
-          id={2}
-          data={props.data[2]}
-          isUsingMetric={props.isUsingMetric}
-          handleClick={handleClick}
-        />
-        <DailyBox
-          id={3}
-          data={props.data[3]}
-          isUsingMetric={props.isUsingMetric}
-          handleClick={handleClick}
-        />
-        <DailyBox
-          id={4}
-          data={props.data[4]}
-          isUsingMetric={props.isUsingMetric}
-          handleClick={handleClick}
-        />
-        <DailyBox
-          id={5}
-          data={props.data[5]}
-          isUsingMetric={props.isUsingMetric}
-          handleClick={handleClick}
-        />
-        <DailyBox
-          id={6}
-          data={props.data[6]}
-          isUsingMetric={props.isUsingMetric}
-          handleClick={handleClick}
-        />
-      </div>
+      <div className="columns is-gapless">{dailyBoxes}</div>
       {extendedBox}
     </div>
   );
